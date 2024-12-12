a713f29d10acfbb88eeb4311e9b540b907752c64
Grand <monyx5861@gmail.com>
2024-12-12 09:27:32 +0100

    linter fixed

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ import chalk from 'chalk'
 
 console.log(chalk.blue('It works'))
 
-let myUnusedVariable = 0
+const myUnusedVariable = 0
+print('now used: ' + myUnusedVariable)
 
 const number = 5;
-if (number == 5) console.log(number);
+if (number === 5) console.log(number);
 
